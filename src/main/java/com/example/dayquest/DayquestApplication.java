35eea284368d43033c2d66f6d6604a3fe641664e
Luc8020 <roveratiluca30@gmail.com>
2024-08-23 20:33:32 +0200

    wordFilter

--- src/main/java/com/example/dayquest/DayquestApplication.java
+++ src/main/java/com/example/dayquest/DayquestApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DayquestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DayquestApplication.class, args);
 	}
---
+++ src/test/java/com/example/dayquest/DayquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dayquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
