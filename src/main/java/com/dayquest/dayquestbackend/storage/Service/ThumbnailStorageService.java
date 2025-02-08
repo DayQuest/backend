1824a687053241eeb2c75fa7914ea1dd4e272f4e
Luc8020 <roveratiluca30@gmail.com>
2025-02-08 02:36:17 +0100

    fix thumbnail loading

--- src/main/java/com/dayquest/dayquestbackend/storage/Service/ThumbnailStorageService.java
+++ src/main/java/com/dayquest/dayquestbackend/storage/Service/ThumbnailStorageService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.InputStream;
+
 @Service
 public class ThumbnailStorageService {
     @Value("${minio.thumbnailBucket}")
@@ -15,15 +17,17 @@ public class ThumbnailStorageService {
     private MinioClient minioClient;
 
     public byte[] getThumbnail(String key) {
-        try {
-            GetObjectArgs args = GetObjectArgs.builder()
-                    .bucket(bucket)
-                    .object(key)
-                    .build();
-            return minioClient.getObject(args).readAllBytes();
+        try (InputStream inputStream = minioClient.getObject(
+                GetObjectArgs.builder()
+                        .bucket(bucket)
+                        .object(key + ".jpg")
+                        .build()
+        )) {
+            return inputStream.readAllBytes();
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
+
 }
