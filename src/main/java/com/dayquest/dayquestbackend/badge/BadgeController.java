fd84c10f10b21a771b0c44f908d9defe4892ffc6
Luc8020 <roveratiluca30@gmail.com>
2025-04-24 17:21:38 +0200

    import page request

--- src/main/java/com/dayquest/dayquestbackend/badge/BadgeController.java
+++ src/main/java/com/dayquest/dayquestbackend/badge/BadgeController.java
@@ -2,6 +2,7 @@ package com.dayquest.dayquestbackend.badge;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
